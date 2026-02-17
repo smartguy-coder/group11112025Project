7b9f9e487363a5e2ddc52fcb0df1de33551aa885
vasylkartychak <w.i.k.mailua@gmail.com>
2026-02-17 20:19:27 +0200

    add bootstrap

--- frontend/app/routers.py
+++ frontend/app/routers.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
+templates = Jinja2Templates(directory='templates')
+
 
 @router.get("/")
-async def index():
-    return 'bla'
+async def index(requests: Request):
+    context = {
+        'request': requests
+    }
+    response = templates.TemplateResponse('pages/index.html', context=context)
+
+
+    return response

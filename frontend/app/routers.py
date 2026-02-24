89dd5218a5a0657c191361b34e5d40aaf1c843e9
vasylkartychak <w.i.k.mailua@gmail.com>
2026-02-24 19:53:20 +0200

    register form

--- frontend/app/routers.py
+++ frontend/app/routers.py
@@ -15,3 +15,15 @@ async def index(requests: Request):
 
 
     return response
+
+
+@router.get("/sign-up")
+@router.post("/sign-up")
+async def user_register(requests: Request):
+    context = {
+        'request': requests
+    }
+    response = templates.TemplateResponse('pages/sign-up.html', context=context)
+
+
+    return response

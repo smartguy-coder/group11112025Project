533f44291dbcd98cc413963efeb440b2dadd69da
vasylkartychak <w.i.k.mailua@gmail.com>
2026-03-03 19:38:41 +0200

    static files

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from routers import router
 
@@ -8,6 +9,9 @@ def get_application() -> FastAPI:
         debug=True
     )
     _app.include_router(router)
+
+    _app.mount('/static', StaticFiles(directory="static"), name='static')
+
     return _app
 
 

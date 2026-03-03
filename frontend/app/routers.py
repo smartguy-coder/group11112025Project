7b2b48813f7266becb85f9dab71d0046fc23e98c
vasylkartychak <w.i.k.mailua@gmail.com>
2026-03-03 20:26:10 +0200

    frontend user login

--- frontend/app/routers.py
+++ frontend/app/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Form, status
+from fastapi import APIRouter, Request, Form, status, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 import httpx
@@ -8,13 +8,30 @@ router = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 
+async def get_user(requests: Request) -> dict:
+    access_token = requests.cookies.get('access_token')
+    if not access_token:
+        return {}
+    async with httpx.AsyncClient() as client_login:
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        response_login = await client_login.get('http://backend:8000/users/my-info', headers=headers)
+        if response_login.status_code == 200:
+            return response_login.json()
+        return {}
+
+
 @router.get("/")
-async def index(requests: Request):
+async def index(requests: Request, user: dict = Depends(get_user)):
     context = {
+        "user": user,
         'request': requests
     }
     response = templates.TemplateResponse('pages/index.html', context=context)
-
 
     return response
 
@@ -44,6 +61,19 @@ async def user_register(requests: Request, username: str = Form(""), email: str 
 
         if response.status_code == status.HTTP_201_CREATED:
             redirect_response = RedirectResponse(requests.url_for("index"), status_code=status.HTTP_303_SEE_OTHER)
+
+            async with httpx.AsyncClient() as client_login:
+                headers = {
+                    'accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+
+                json_data = {
+                    'password': password,
+                    'username': email,
+                }
+                response_login = await client_login.post('http://backend:8000/users/login', data=json_data, headers=headers)
+            redirect_response.set_cookie('access_token', response_login.json()['access_token'], max_age=15*60)
             return redirect_response
         elif response.status_code == status.HTTP_409_CONFLICT:
             context['username'] = username
